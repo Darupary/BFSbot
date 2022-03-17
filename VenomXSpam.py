57320e31f3474d82bfb1043cf0bba94f79cb8644
MK <101259067+ERR0rMK@users.noreply.github.com>
2022-03-17 16:21:55 +0530

    Update VenomXSpam.py

--- VenomXSpam.py
+++ VenomXSpam.py
@@ -1450,8 +1450,7 @@ async def ping(e):
         event = await e.reply(text, parse_mode=None, link_preview=None )
         end = datetime.now()
         ms = (end-start).microseconds / 1000
-        await event.edit(f"⚡🇬 🇦 🇳 🇩 .🇴 🇵⚡\n\n⍣⃟⏤͟͟͞͞★🇹 🇺 🇫 🇦 🇦 🇳=͟͟͞͞𐏓⚡  🥳 X sᴘᴀᴍ ϟ `{ms}` ᴍs")
-
+        await event.edit("🤤 🇬 🇦 🇳 🇩 . 🇴 🇵🤤\n\n🤩=͟͟͞͞⚡🇹 🇺 🇫 🇦 🇦 🇳=͟͟͞͞⚡🤩 X sᴘᴀᴍ ϟ `{ms}` ᴍs")
 
     
         
